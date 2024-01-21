2be04659de94ffc137b7c2a2d52c1276011c3692
DarshanaParthiban <darshanapr.21ece@kongu.edu>
2024-01-21 21:11:20 +0530

    Darshana

---
+++ intro.js
@@ -0,0 +1,50 @@
+console.log("you")
+//variables
+a='a'
+console.log("General declaration of variable",a);
+var b="Hii";
+console.log("This variable belongs to var type value is",b)
+var b=10;
+console.log("This variable belongs to changed var type value is",b)
+let c='Hello';
+console.log("This variable belongs to let type value is",c)
+const d=2000;
+console.log("This variable belongs to const type value is",d)
+//d=200;
+//console.log("This variable belongs to const type value is",d)
+
+
+//Assignament operator
+var a=10
+console.log("Assignment operator output",a)
+//Arithmetic operator
+var aA=10
+var bA=10
+console.log(typeof(aA))
+console.log("Arithmetic operator addition output",aA+bA)
+console.log("Arithmetic operator  subraction output",aA-bA)
+console.log("Arithmetic operator multiplication output",aA*bA)
+console.log("Arithmetic operator division output",aA/bA)
+console.log("Arithmetic operator modulus output",aA%bA)
+console.log("Arithmetic operator power output",aA**bA)
+console.log("Arithmetic operator Increment output",aA++)
+console.log("Arithmetic operator Decrement output",--bA)
+console.log("Arithmetic operator Increment output",aA++)
+
+//logical operator-> &&,||,!
+ var aL=10
+ var bL=10
+ console.log("logical operator AND output",aL&&bL)
+ console.log("logical operator || output",aL||bL)
+ console.log("logical operator ! output",!aL)
+ console.log(10&&20)
+ console.log(10&&30)
+ console.log(10||20)
+ console.log(0||20)
+ console.log(0||20)
+ console.log(100||20&&0)
+ console.log((100||20)&&0)
+ console.log(0&&(100||20))
+ console.log(0&&100||20)
+
+ 
